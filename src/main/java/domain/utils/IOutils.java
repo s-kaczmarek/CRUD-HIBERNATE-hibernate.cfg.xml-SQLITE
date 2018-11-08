540f865e6e9cf3d9fae9a6d16883ce666f71904e
Sławek <skaczmarek88@gmail.com>
2018-11-08 19:30:16 +0100

    misc updates before branching

--- src/main/java/domain/utils/IOutils.java
+++ src/main/java/domain/utils/IOutils.java
@@ -4,33 +4,29 @@ import domain.Contact;
 import domain.ContactRepository;
 import domain.utils.helpers.CSVHelper;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class IOutils {
 
-    public static void saveAllContactsToCSV() throws Exception {
+    public static void saveAllContactsToCSV(String filePath) throws Exception {
 
-        List<Contact> allContactsList = null;
-        allContactsList = ContactRepository.listAllContacts();
+        List<Contact> allContactsList = ContactRepository.listAllContacts();
 
         for(Contact contact : allContactsList){
             List<String> stringListToAdd = new ArrayList<String>();
             stringListToAdd.add(String.valueOf(contact.getContactId()));
             stringListToAdd.add(contact.getFirstName());
             stringListToAdd.add(contact.getLastName());
-            //stringListToAdd.add(String.valueOf(contact.getEmail()));
-            //stringListToAdd.add(String.valueOf(contact.getGroups()));
+            stringListToAdd.add(String.valueOf(contact.getEmail().getEmail()));
+            stringListToAdd.add(String.valueOf(contact.getGroups()));
 
-            System.out.println(stringListToAdd);
+            //System.out.println(stringListToAdd);
 
-            String filePathWindows = "D:\\CLOUD\\MEGA\\Documents\\#PROGRAMOWANIE\\JAVA\\Brudnopisy\\CRUD-HIBERNATE-hibernate.cfg.xml-SQLITE\\test.csv";
+            //String filePathWindows = "D:\\CLOUD\\MEGA\\Documents\\#PROGRAMOWANIE\\JAVA\\Brudnopisy\\CRUD-HIBERNATE-hibernate.cfg.xml-SQLITE\\test.csv";
 
-            File csvFile = new File(filePathWindows);
+            File csvFile = new File(filePath);
             FileOutputStream fos = new FileOutputStream(csvFile, true);
             Writer fw = new OutputStreamWriter(fos, "UTF-8");
             CSVHelper.writeLine(fw, stringListToAdd);
@@ -39,12 +35,15 @@ public class IOutils {
 
             // TODO wyciągląc grupy z listy contacts i dodać osobno do listy stringListToAdd
         }
-
-
     }
 
-    public static void loadCSVtoDatabase(String pathToFile){
+    public static List<String> loadContactsFromCSV(String filePath) throws Exception {
 
+        List<String> allStringsFromCSV;
+        Reader fr = new FileReader(filePath);
+        allStringsFromCSV = CSVHelper.parseLine(fr);
+
+        return allStringsFromCSV;
 
     }
 }
--- src/test/java/domain/utils/IOutilsTest.java
+++ src/test/java/domain/utils/IOutilsTest.java
@@ -1,12 +1,40 @@
 package domain.utils;
 
+import domain.Contact;
+import domain.ContactRepository;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static domain.utils.IOutils.loadContactsFromCSV;
+
 public class IOutilsTest {
+
+    private static String filePath = "D:\\CLOUD\\MEGA\\Documents\\#PROGRAMOWANIE\\JAVA\\Brudnopisy\\CRUD-HIBERNATE-hibernate.cfg.xml-SQLITE\\test.csv";
+
+    @Test
+    public void shouldAddAllRecordsToCSV() throws Exception {
+
+        List<Contact> contactsFromDatabase = ContactRepository.listAllContacts();
+
+        if(contactsFromDatabase == null){
+            contactsFromDatabase = SampleDataUtils.generateTestData();
+            ContactRepository.addNewContactsFromList(contactsFromDatabase);
+        }
+
+        IOutils.saveAllContactsToCSV(filePath);
+        List<String> contactsFromCSV = IOutils.loadContactsFromCSV(filePath);
+        System.out.println("List of contacts: " + contactsFromCSV);
+        Assert.assertEquals(contactsFromDatabase, contactsFromCSV);
+
+    }
 
     // TODO
     public static void main(String[] args) {
 
         try {
-            IOutils.saveAllContactsToCSV();
+            IOutils.saveAllContactsToCSV(filePath);
         } catch (Exception e) {
             e.printStackTrace();
         }
